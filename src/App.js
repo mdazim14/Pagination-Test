2fa21358ee474ca3470df09ebd0a4d1030526137
Md Azim <mdazim.cse@gmail.com>
2021-08-23 03:10:27 +0600

    done

--- src/App.js
+++ src/App.js
@@ -4,53 +4,76 @@ import fakeData from './jsonData';
 import { useEffect, useState } from 'react';
 
 function App() {
-  
+
   const ItemsNumber = 5;
 
   const [allItems, setAllItems] = useState([])
   // console.log("all data after set", allItems)
-  const dataLength = allItems.length;
-  let pageCount = Math.ceil(dataLength/ItemsNumber);
-// console.log("page count", pageCount)
+  const [actvieEl, setActiveEl] = useState(1);
+  const [newItems, setNewItems] = useState([])
 
+
+  const dataLength = allItems.length;
+  let pageCount = Math.ceil(dataLength / ItemsNumber);
+  // console.log("page count", pageCount)
 
   useEffect(() => {
     const data = fakeData?.data;
     setAllItems(data)
     // console.log("all data", data)
+    
+    const newArray = data.slice(0,ItemsNumber);
+    setNewItems(newArray);
 
   }, [])
 
-  let active = 1;
+  let active = actvieEl;
   let items = [];
   for (let number = 1; number <= pageCount; number++) {
     items.push(
-      <Pagination.Item key={number} active={number === active}>
+      <Pagination.Item onClick={()=>{pageClick(number)}} key={number} active={number === active}>
         {number}
       </Pagination.Item>,
     );
   }
+
+  const pageClick = (value)=>{
+    console.log('pageClicked',value)
+    setActiveEl(value)
+    
+    const lastIndex = (value*ItemsNumber);
+    console.log('last',lastIndex);
+
+    const firstIndex =  lastIndex-ItemsNumber;
+    console.log('first',firstIndex);
+
+    const newArray = allItems.slice(firstIndex,lastIndex);
+    setNewItems(newArray);
+  
+  }
+
   return (
     <div>
-      <h1>New Pagination AZIM</h1>
+      <h1 className="text-center">New Pagination AZIM</h1>
       <div className="border d-flex justify-content-center mt-5 mb-5">
         <Pagination>{items}</Pagination>
       </div>
-      <div className="border container mt-5">
-     {
-       // eslint-disable-next-line array-callback-return
-       allItems.map((item=>{
-         return(
-         <div className="border p-5 mt-5">
 
-           <h2>{item.id}. {item.title}</h2>
-           <p>{item.desc}</p>
-           <h5>{item.age}</h5>
-         </div>
-         )
-       }))
-     }
-    </div>
+      <div className="border container mt-5">
+        {
+          // eslint-disable-next-line array-callback-return
+          newItems.map((item => {
+            return (
+              <div className="border p-5 mt-5">
+
+                <h2>{item.id}. {item.title}</h2>
+                <p>{item.desc}</p>
+                <h5>{item.age}</h5>
+              </div>
+            )
+          }))
+        }
+      </div>
     </div>
   );
 }
